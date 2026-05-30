c1d46786fec83fb04e074c16c243bc30123b1d36
magzog <sing@magzog.com>
2026-05-30 14:43:07 +1200

    Add comments

--- packages/core/src/common/models.ts
+++ packages/core/src/common/models.ts
@@ -1,66 +1,125 @@
-// import { Book } from "../../core/src/book";
-// import { BookShelfManager } from "../../core/src/bookShelfManager";
-// import { BookViewer } from "../../core/src/bookViewer";
 import { IBookSize, ISize } from "./dimension";
 import { Rect } from "./shape";
 
-
+/**
+ * Deeply required type.
+ */
 export type DeepRequired<T> = Required<{
   [K in keyof T]: T[K] extends Required<T[K]> ? T[K] : DeepRequired<T[K]>
 }>
-
+/**
+ * Publication data.
+ * 하나의 책에 들어갈 수 있는 출판사 정보.
+ */
 export interface IPublication {
+  /**
+   * Name of the publication.
+   */
   name: string;
+  /**
+   * Location of the publication.
+   */
   location: string;
+  /**
+   * Published date of the publication.
+   */
   publishedDate: string;
 }
-
+/**
+ * Page type.
+ */
 export enum PageType {
   Page = "Page",
   Cover = "Cover",
   Empty = "Empty",
   Blank = "Blank",
 }
-
+/**
+ * Page label type.
+ */
 export enum PageLabelType {
   Default = "Default",
   Empty = "Empty",
 }
-
-
+/**
+ * Default size of the book.
+ */
 export enum DefaultSize {
   bookWidth = 600,
   bookHeight = 900,
   pageWidth = 600,
   pageHeight = 900,
 }
-
+/**
+ * Book type.
+ */
 export enum BookType {
   Book = "Book",
   Magazine = "Magazine",
   Newspaper = "Newspaper",
 }
-
+/**
+ * Book status.
+ */
 export enum BookStatus {
   Open = "Open",
   Close = "Close",
 }
-
+/**
+ * Flipping event status.
+ * TODO: FlipEventStatus 로 변경 필요.
+ * TODO: FlipView 패키지로 옮길 필요 있음.
+ */
 export enum EventStatus {
+  /**
+   * No event.
+   */
   None = 0b0000_0000,
-  
+  /**
+   * Auto flip event.
+   * 페이지 자동 넘기기 진행중
+   */
   AutoFlip = 0b0000_1000,
+  /**
+   * FlipZone 영역에 마우스가 들어가 있는 상태(MouseEnter)로
+   * 이때 페이지의 코너가 마우스 포인트 위치로 따라 다니는 애니메이션 진행중
+   */
   AutoFlipFromCorner = 0b0000_1100,
+  /**
+   * FlipZone 영역에 마우스가 나온 상태(MouseLeave)로
+   * 마우스를 따라다니던 페이지의 코너가 원래 위치로 돌아가는 애니메이션 진행중
+   */
   AutoFlipToCorner = 0b0000_1010,
-  
+  /**
+   * Flipping event.
+   * 자동으로 책장이 넘어가는 애니메이션이 진행중인 상태.
+   */
   Flipping = 0b1000_0000,
+  /**
+   * 페이지를 넘기기 위해 마우스로 드래그 하다가 마우스를 놓은 상태(MouseUp)로
+   * 원래 위치로 페이지가 자동으로 넘어가는 애니메이션 진행중
+   */
   SnappingBack = 0b1001_0000,
+  /**
+   * 페이지를 넘기기 위해 마우스로 드래그 하다가 마우스를 놓은 상태(MouseUp)로
+   * Page Index 가 높아지는 방향의 페이지로 페이지가 자동으로 넘어가는 애니메이션 진행중
+   */
   FlippingForward = 0b1010_0000,
+  /**
+   * 페이지를 넘기기 위해 마우스로 드래그 하다가 마우스를 놓은 상태(MouseUp)로
+   * Page Index 가 낮아지는 방향의 페이지로 페이지가 자동으로 넘어가는 애니메이션 진행중
+   */
   FlippingBackward = 0b1100_0000,
-  
+  /**
+   * 페이지를 넘기기 위해 MouseDown 한 상태 또는 MouseDown 후 드래그 하는 상태
+   */
   Dragging = 0b1000_0000_0000,
 }
-
+/**
+ * Flipping 을 위한 이벤트 Zone.
+ * TODO: 이름을 FlipEventZone 으로 변경 필요.
+ * TODO: Zone 패키지로 옮길 필요 있음.
+ */
 export enum Zone {
   LT=0b0100_0010,
   LC=0b0010_0010,
@@ -74,12 +133,25 @@ export enum Zone {
   Center=0b0010_0000,
   Bottom=0b0001_0000,
 }
-
+/**
+ * Flipping Zone 에 마우스가 있거나 마우스가 해당 Zone 을 나가능 경우
+ * 발생하는 Auto Flip 효과의 종류
+ */
 export enum AutoFlipType {
+  /**
+   * 마우스가 Flipping Zone 들어 왔을 때 
+   * 해당 코너가 고정된 크기로 정해진 위치까지 Flipping 하는 타입
+   */
   FixedWidth=0,
+  /**
+   * 마우스가 Flipping Zone 들어 왔을 때 
+   * 해당 코너가 마우스 포인트 위치까지 Flipping 하는 타입
+   */
   MouseCursor=1,
 }
-
+/**
+ * Viewer type.
+ */
 export enum ViewerType {
   Flipping="flipping",
   Scrolling="scrolling"
@@ -160,39 +232,79 @@ export interface IBookEl {
 export interface IPageLabel extends IPageLabelData, IPageLabelEl {
   setEvents():void;
 }
+/**
+ * Data of the page label.
+ */
 export interface IPageLabelData {
-  index: number;    // index of the label
-  pageIndex: number; // index of the page in the book
+  /**
+   * Index of the label.
+   */
+  index: number;
+  /**
+   * Index of the page in the book.
+   */
+  pageIndex: number;
+  /**
+   * Type of the label.
+   */
   type?: PageLabelType;
-  size?: ISize;     // size of the label sub container
+  /**
+   * Size of the label sub container.
+   */
+  size?: ISize;
+  /**
+   * Top position of the label sub container.
+   */
   top?: number;
-  ignore?: boolean; // ignore the page when displaying the book
+  /**
+   * Whether to ignore the page label when displaying the book.
+   */
+  ignore?: boolean;
+  /**
+   * Content of the label.
+   */
   content?: any;
+  /**
+   * Background color of the label.
+   */
   backgroundColor?: string;
+  /**
+   * Opacity of the label.
+   */
   opacity?: number | string;
+  /**
+   * Click event handler of the label.
+   */
   onClick?: (pageIndex: number) => void;
 }
-
+/**
+ * Element of the page label.
+ */
 export interface IPageLabelEl {
   readonly element: HTMLElement;
   // readonly contentContainerEl: HTMLElement;
   readonly contentEl: HTMLElement;
   resetLabelEls():void;
 }
-
-
+/**
+ * Page class
+ */
 export interface IPage extends IPageData, IPageEl {
   size: ISize;
   setEvents():void;
 }
-
+/**
+ * Element of the page.
+ */
 export interface IPageEl {
   readonly element: HTMLElement;
   readonly contentContainerEl: HTMLElement;
   readonly contentEl: HTMLElement;
   resetPageEls():void;
 }
-
+/**
+ * Data of the page.
+ */
 export interface IPageData {
   id: string;
   type?: PageType;
